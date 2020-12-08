723165caa3e6588e8aee53350b190fa0918cc428
Mike Cousins <mike@mikecousins.com>
2020-12-07 22:08:17 -0700

    Adding an aria-label to the theme toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -9,10 +9,10 @@ const ThemeToggle = () => {
 
   return (
     <span
-      id="theme-toggle"
       role="checkbox"
       tabIndex={0}
       aria-checked="false"
+      aria-label="Theme Toggle"
       className={clsx(theme === 'light' ? 'bg-gray-200' : 'bg-indigo-600', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline float-right')}
       onClick={toggleTheme}
     >
