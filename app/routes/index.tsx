b15f73bbb1e7c3e5ace3f24dcc5170ee412a301d
Mike Cousins <mike@mikecousins.com>
2021-12-02 00:32:45 -0700

    Adding production environment variable to build

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,57 +1,52 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import type { MetaFunction } from "remix";
 import IconLink from '~/components/IconLink';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import ThemeToggle from '~/components/ThemeToggle';
 
-// https://remix.run/api/conventions#meta
 export let meta: MetaFunction = () => {
   return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
+    title: "Mike Cousins",
+    description: "The personal site of Mike Cousins"
   };
 };
 
-// https://remix.run/guides/routing#index-routes
-export default function Index() {
-  let data = useLoaderData<IndexData>();
+const Index = () => (
+  <>
+    <h1 className="text-6xl font-serif leading-none mb-2">Mike Cousins</h1>
+    <p className="font-mono">
+      Software professional specializing in front end development, UX and team leadership. React/Typescript, Swift, Kotlin. Crafting modern, simple, user-focused software.
+    </p>
+    <div className="text-xl mt-4">
+      <IconLink
+        href="https://github.com/mikecousins"
+        title="Github"
+        first
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </IconLink>
+      <IconLink
+        href="https://twitter.com/mikecousins"
+        title="Twitter"
+      >
+        <FontAwesomeIcon icon={faTwitter} />
+      </IconLink>
+      <IconLink
+        href="https://www.linkedin.com/in/mikecousins/"
+        title="LinkedIn"
+      >
+        <FontAwesomeIcon icon={faLinkedin} />
+      </IconLink>
+      <IconLink
+        href="mailto:mike@mikecousins.com"
+        title="Email"
+      >
+        <FontAwesomeIcon icon={faEnvelope} />
+      </IconLink>
+      <ThemeToggle />
+    </div>
+  </>
+);
 
-  return (
-    <>
-      <h1 className="text-6xl font-serif leading-none mb-2">Mike Cousins</h1>
-      <p className="font-mono">
-        Software professional specializing in front end development, UX and team leadership. React/Typescript, Swift, Kotlin. Crafting modern, simple, user-focused software.
-      </p>
-      <div className="text-xl mt-4">
-        <IconLink
-          href="https://github.com/mikecousins"
-          title="Github"
-          first
-        >
-          <FontAwesomeIcon icon={faGithub} />
-        </IconLink>
-        <IconLink
-          href="https://twitter.com/mikecousins"
-          title="Twitter"
-        >
-          <FontAwesomeIcon icon={faTwitter} />
-        </IconLink>
-        <IconLink
-          href="https://www.linkedin.com/in/mikecousins/"
-          title="LinkedIn"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </IconLink>
-        <IconLink
-          href="mailto:mike@mikecousins.com"
-          title="Email"
-        >
-          <FontAwesomeIcon icon={faEnvelope} />
-        </IconLink>
-        <ThemeToggle />
-      </div>
-    </>
-  );
-}
+export default Index;
