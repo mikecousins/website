d84d767cf09758d293e9064aec628b91c02c0972
Mike Cousins <mike@mikecousins.com>
2022-07-08 18:11:22 -0600

    Updating colours

--- app/layouts/index.tsx
+++ app/layouts/index.tsx
@@ -8,9 +8,12 @@ const Layout: FunctionComponent = ({ children }) => {
   return (
     <div className={clsx(
       'flex h-screen transition-colors ease-in-out duration-1000',
-      theme === 'light' ? 'text-gray-800 bg-gray-50' : ' text-gray-100 bg-gray-700'
+      theme === 'light' ? 'bg-gradient-to-r from-cyan-100 to-blue-100' : 'bg-gradient-to-r from-cyan-900 to-blue-900'
     )}>
-      <div className="p-2 mx-auto my-auto max-w-2xl">
+      <div className={clsx(
+        'p-4 mx-auto my-auto max-w-2xl rounded-xl transition-colors ease-in-out duration-1000 bg-opacity-20',
+        theme === 'light' ? 'text-gray-800 bg-white' : 'text-gray-100 bg-black'
+      )}>
         {children}
       </div>
     </div>
