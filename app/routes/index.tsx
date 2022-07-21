9712b25de385a78db5b1331bbee19054809e9a0d
Mike Cousins <mike@mikecousins.com>
2022-07-20 22:58:06 -0600

    Moving to latest yarn

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,18 @@
 import type { MetaFunction } from "remix";
-import IconLink from '~/components/IconLink';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import ThemeToggle from '~/components/ThemeToggle';
+import IconLink from "../components/IconLink";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import ThemeToggle from "../components/ThemeToggle";
 
 export let meta: MetaFunction = () => {
   return {
     title: "Mike Cousins",
-    description: "The personal site of Mike Cousins"
+    description: "The personal site of Mike Cousins",
   };
 };
 
@@ -16,20 +20,15 @@ const Index = () => (
   <>
     <h1 className="text-6xl font-serif leading-none mb-2">Mike Cousins</h1>
     <p className="font-mono">
-      Software professional specializing in front end development, UX and team leadership. React/Typescript, Swift, Kotlin. Crafting modern, simple, user-focused software.
+      Software professional specializing in front end development, UX and team
+      leadership. React/Typescript, Swift, Kotlin. Crafting modern, simple,
+      user-focused software.
     </p>
     <div className="text-xl mt-4 flex">
-      <IconLink
-        href="https://github.com/mikecousins"
-        title="Github"
-        first
-      >
+      <IconLink href="https://github.com/mikecousins" title="Github" first>
         <FontAwesomeIcon icon={faGithub} />
       </IconLink>
-      <IconLink
-        href="https://twitter.com/mikecousins"
-        title="Twitter"
-      >
+      <IconLink href="https://twitter.com/mikecousins" title="Twitter">
         <FontAwesomeIcon icon={faTwitter} />
       </IconLink>
       <IconLink
@@ -38,10 +37,7 @@ const Index = () => (
       >
         <FontAwesomeIcon icon={faLinkedin} />
       </IconLink>
-      <IconLink
-        href="mailto:mike@mikecousins.com"
-        title="Email"
-      >
+      <IconLink href="mailto:mike@mikecousins.com" title="Email">
         <FontAwesomeIcon icon={faEnvelope} />
       </IconLink>
       <div className="flex-1" />
