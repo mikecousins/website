3a43fafa5c582deb41dfbeb8e227c6f1e0cb25b9
Mike Cousins <mike@mikecousins.com>
2022-07-31 18:29:34 -0600

    Adding more posts

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,13 +1,33 @@
-import { json } from "@remix-run/node"; // or cloudflare/deno
-import { Link, useLoaderData } from "@remix-run/react";
+import { json } from '@remix-run/node'; // or cloudflare/deno
+import { Link, useLoaderData } from '@remix-run/react';
+import { compareAsc, parseISO } from 'date-fns';
 
-import * as postA from "./dr-gillis-1.md";
+import * as drGillis1 from './dr-gillis-1.md';
+import * as drGillis2 from './dr-gillis-2.md';
+import * as fasting from './fasting.md';
+import * as finished from './finished-but-crying.md';
+import * as firstVisit from './first-visit-to-tom-baker.md';
 
-function postFromModule(mod: any) {
+type Meta = {
+  title: string;
+  slug: string;
+  description: string;
+  date: Date;
+};
+
+type Attributes = {
+  meta: Meta;
+};
+
+type Module = {
+  attributes: Attributes;
+};
+
+const postFromModule = (mod: Module): Meta => {
   return {
     ...mod.attributes.meta,
   };
-}
+};
 
 export async function loader() {
   // Return metadata about each of the posts for display on the index page.
@@ -15,23 +35,27 @@ export async function loader() {
   // lets us avoid bundling the actual posts themselves in the bundle for the
   // index page.
   return json([
-    postFromModule(postA),
+    postFromModule(drGillis1),
+    postFromModule(drGillis2),
+    postFromModule(fasting),
+    postFromModule(firstVisit),
+    postFromModule(finished),
   ]);
 }
 
 export default function Index() {
-  const posts = useLoaderData();
+  const posts = useLoaderData<Meta[]>();
 
   return (
     <ul>
-      {posts.map((post: any) => (
-        <li key={post.slug}>
-          <Link to={post.slug}>*{post.title}*</Link>
-          {post.description ? (
-            <p>{post.description}</p>
-          ) : null}
-        </li>
-      ))}
+      {posts
+        .sort((a, b) => compareAsc(parseISO(a.date), parseISO(b.date)))
+        .map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>*{post.title}*</Link>
+            {post.description ? <p>{post.description}</p> : null}
+          </li>
+        ))}
     </ul>
   );
 }
