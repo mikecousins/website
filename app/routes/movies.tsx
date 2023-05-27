179f8c64657294e5eae1e71aacf870e11820367d
Mike Cousins <mike@mikecousins.com>
2023-05-27 10:33:26 -0600

    Simplifying movie stats

--- app/routes/movies.tsx
+++ app/routes/movies.tsx
@@ -14,14 +14,6 @@ export const meta: V2_MetaFunction = () => {
 
 const Movies = () => {
   const watched = movies.filter((movie) => !!movie.watched).length;
-  const collected = movies.filter((movie) => !!movie.resolution).length;
-  const perfect = movies.filter(
-    (movie) =>
-      movie.resolution === '4k' &&
-      movie.source === 'UHD BD' &&
-      movie.remux &&
-      movie.dolbyVision
-  ).length;
 
   return (
     <Layout>
@@ -35,39 +27,8 @@ const Movies = () => {
               Bucket list endeavour to watch all of the best movies ever made.
             </p>
           </div>
-          <div className="mt-10 pb-12 sm:pb-16 font-mono">
-            <div className="relative">
-              <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="mx-auto max-w-4xl">
-                  <dl className="sm:grid sm:grid-cols-3">
-                    <div className="flex flex-col border-b border-gray-900 p-6 text-center sm:border-0 sm:border-r">
-                      <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-300">
-                        Watched
-                      </dt>
-                      <dd className="order-1 text-5xl font-bold tracking-tight text-orange-600">
-                        {watched}
-                      </dd>
-                    </div>
-                    <div className="flex flex-col border-t border-b border-gray-900 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                      <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-300">
-                        Collected
-                      </dt>
-                      <dd className="order-1 text-5xl font-bold tracking-tight text-orange-600">
-                        {collected}
-                      </dd>
-                    </div>
-                    <div className="flex flex-col border-t border-gray-900 p-6 text-center sm:border-0 sm:border-l">
-                      <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-300">
-                        Perfect Quality
-                      </dt>
-                      <dd className="order-1 text-5xl font-bold tracking-tight text-orange-600">
-                        {perfect}
-                      </dd>
-                    </div>
-                  </dl>
-                </div>
-              </div>
-            </div>
+          <div className="my-8 font-mono text-4xl">
+            Watched: {watched} / 250
           </div>
         </div>
         <table
