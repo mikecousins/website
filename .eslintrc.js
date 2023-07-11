7548d2f118a5bb4ec59ff5e36d6f986ba3149abb
Mike Cousins <mike@mikecousins.com>
2023-07-10 22:21:59 -0700

    Simplifying eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,9 +20,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
     'plugin:jsx-a11y/recommended',
+    '@remix-run/eslint-config',
   ],
   rules: {
     'react/react-in-jsx-scope': 'off', // need this for vite support
