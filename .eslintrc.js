0b987cf4b681abb1cdee864c79c5657fb6a68d1d
Mike Cousins <mike@mikecousins.com>
2023-07-11 10:49:03 -0700

    Removing a11y eslint package because it's in remix one

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,11 +16,5 @@ module.exports = {
       version: 'detect',
     },
   },
-  extends: [
-    'plugin:jsx-a11y/recommended',
-    '@remix-run/eslint-config',
-  ],
-  rules: {
-    'react/react-in-jsx-scope': 'off', // need this for vite support
-  },
+  extends: '@remix-run/eslint-config',
 };
