16176b55a3fa2e64d20a26866459e2bce66d686b
Mike Cousins <mike@mikecousins.com>
2023-08-24 11:24:12 -0600

    Lint fix

--- app/routes/work.tsx
+++ app/routes/work.tsx
@@ -1,4 +1,4 @@
-import { V2_MetaFunction } from '@remix-run/node';
+import { type V2_MetaFunction } from '@remix-run/node';
 import Layout from '~/layouts';
 
 export const meta: V2_MetaFunction = () => {
