df86cb48be13a13000ab66397d74b9aa48fe0300
Mike Cousins <mike@mikecousins.com>
2024-07-23 16:55:14 -0600

    Changing slug param syntax

--- app/routes/projects_.$slug.tsx
+++ app/routes/projects_.$slug.tsx
@@ -1,34 +1,24 @@
 import { useMemo } from 'react';
 import { json, useLoaderData } from '@remix-run/react';
 import { getMDXComponent } from 'mdx-bundler/client/index.js';
-import { getProject, type Frontmatter } from '~/utilities/project.server';
 import type { LoaderFunctionArgs } from '@remix-run/node';
+import invariant from 'tiny-invariant';
+import { getProject } from '~/utilities/project.server';
 import { ArticleLayout } from '~/components/ArticleLayout';
 import { Layout } from '~/components/Layout';
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const slug = params['*'];
-  if (!slug) throw new Response('Not found', { status: 404 });
+  const { slug } = params;
+  invariant(slug, 'Slug is required');
 
   const post = await getProject(slug);
-  if (post) {
-    const { frontmatter, code } = post;
+  invariant(post, 'Post not found');
 
-    console.log(post);
-    return json({ frontmatter, code });
-  } else {
-    throw new Response('Not found', { status: 404 });
-  }
+  const { frontmatter, code } = post;
+  return json({ frontmatter, code });
 };
 
-function PostHeader(props: { frontmatter: Frontmatter }) {
-  const { frontmatter } = props;
-
-  // We can implement whatever we want here
-  return <>{JSON.stringify(frontmatter, null, 2)}</>;
-}
-
-export default function Project() {
+const Project = () => {
   const { code, frontmatter } = useLoaderData<typeof loader>();
   const Component = useMemo(() => getMDXComponent(code), [code]);
 
@@ -39,4 +29,6 @@ export default function Project() {
       </ArticleLayout>
     </Layout>
   );
-}
+};
+
+export default Project;
