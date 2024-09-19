f855ff487ed1786bf372bfde3d0ef6ce38bde14b
Mike Cousins <mike@mikecousins.com>
2024-09-18 18:12:36 -0600

    Updating services page

--- app/routes/services.tsx
+++ app/routes/services.tsx
@@ -17,13 +17,20 @@ const Services = () => (
       <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
         Services
       </h1>
-      <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+      <div className="mt-6 space-y-7">
         <p>
-          I am available for contract work, fractional CTO engagements, and
-          technical consulting. Check out the <Link to="/work">work</Link> that
-          I have done. I have experience in many stages of companies and in many
-          different industries. Reach out if you would like to work together!
+          I am available for contract software development, fractional CTO
+          engagements, and technical consulting. Check out the{' '}
+          <Link
+            to="/projects"
+            className="underline text-zinc-800 dark:text-zinc-100 decoration-cyan-400"
+          >
+            projects
+          </Link>{' '}
+          that I have done. I have experience in many stages of companies and in
+          many different industries.
         </p>
+        <p>Reach out if you would like to work together!</p>
       </div>
     </Container>
   </Layout>
