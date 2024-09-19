0a051bed39cb959e0cc2efd8b1dd68bd3fd04cdd
Mike Cousins <mike@mikecousins.com>
2024-09-19 17:12:56 -0600

    Updating about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@remix-run/react';
+import { Link, type MetaFunction } from '@remix-run/react';
 import clsx from 'clsx';
 import { Container } from '~/components/Container';
 import { Layout } from '~/components/Layout';
@@ -11,7 +11,7 @@ import {
 } from '~/components/SocialIcons';
 import portraitImage from '~/images/elkvtc.jpg';
 
-function SocialLink({
+const SocialLink = ({
   className,
   href,
   children,
@@ -21,30 +21,34 @@ function SocialLink({
   href: string;
   icon: React.ComponentType<{ className?: string }>;
   children: React.ReactNode;
-}) {
-  return (
-    <li className={clsx(className, 'flex')}>
-      <Link
-        to={href}
-        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
-      >
-        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
-        <span className="ml-4">{children}</span>
-      </Link>
-    </li>
-  );
-}
+}) => (
+  <li className={clsx(className, 'flex')}>
+    <Link
+      to={href}
+      className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+    >
+      <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+      <span className="ml-4">{children}</span>
+    </Link>
+  </li>
+);
 
-function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        fillRule="evenodd"
-        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
-      />
-    </svg>
-  );
-}
+const MailIcon = (props: React.ComponentPropsWithoutRef<'svg'>) => (
+  <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+    <path
+      fillRule="evenodd"
+      d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+    />
+  </svg>
+);
+
+export const meta: MetaFunction = () => {
+  return [
+    {
+      title: 'Mike Cousins - About',
+    },
+  ];
+};
 
 const About = () => {
   return (
@@ -63,34 +67,42 @@ const About = () => {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-              I’m Mike Cousins. I live in Calgary, where I help companies start and scale.
+              I’m Mike Cousins. I live in Calgary, where I help companies start
+              and scale.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I started my life in the shadow of my father, a career software
+                developer and ravenous learner. He inspired my love of computers
+                and all kinds of technology. I had a computer in my bedroom by
+                the time I was 6, and I began exploring it with day and night.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                After school I set off on a mission to work in as many different
+                industries as possible. This is one of my favourite aspects of
+                technology, the ability to work in any industry. Since then I
+                have worked in telecommunications, oil and gas, healthcare,
+                finance, energy, and many more.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                I have also done many different roles at those companies from
+                being a pure software developer, to architecting and tech
+                leadership, all the way up to CTO/VP roles. I still don't know
+                what I like best, so I keep doing them all. Maybe when I get
+                older I'll figure it out.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                Along the way I have realized that I love small companies, as I
+                find that I get the most opportunities to make a real impact. I
+                usually get a widely defined role where I get to touch a lot of
+                the business. True happiness for me is learning about a new
+                business, a new industry, meeting new people, and getting to
+                solve a fun problem.
+              </p>
+              <p>
+                Today, I am tackling as many problems as I can get my hands on.
+                I usually have a few things going on at all times to keep me
+                busy.
               </p>
             </div>
           </div>
@@ -99,16 +111,32 @@ const About = () => {
               <SocialLink href="https://x.com/mikecousins" icon={XIcon}>
                 Follow on X
               </SocialLink>
-              <SocialLink href="https://www.instagram.com/mikecousins" icon={InstagramIcon} className="mt-4">
+              <SocialLink
+                href="https://www.instagram.com/mikecousins"
+                icon={InstagramIcon}
+                className="mt-4"
+              >
                 Follow on Instagram
               </SocialLink>
-              <SocialLink href="https://github.com/mikecousins" icon={GitHubIcon} className="mt-4">
+              <SocialLink
+                href="https://github.com/mikecousins"
+                icon={GitHubIcon}
+                className="mt-4"
+              >
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="https://www.linkedin.com/in/mikecousins" icon={LinkedInIcon} className="mt-4">
+              <SocialLink
+                href="https://www.linkedin.com/in/mikecousins"
+                icon={LinkedInIcon}
+                className="mt-4"
+              >
                 Follow on LinkedIn
               </SocialLink>
-              <SocialLink href="https://www.threads.net/@mikecousins" icon={ThreadsIcon} className="mt-4">
+              <SocialLink
+                href="https://www.threads.net/@mikecousins"
+                icon={ThreadsIcon}
+                className="mt-4"
+              >
                 Follow on Threads
               </SocialLink>
               <SocialLink
