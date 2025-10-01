0bc854b78685dbec463b6e7cb7efbc5859f23d78
Mike Cousins <mikecousins@gmail.com>
2025-10-01 13:48:31 -0600

    Adding Netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import mdx from '@mdx-js/rollup';
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 
 export default defineConfig({
-  plugins: [{ enforce: 'pre', ...mdx() }, reactRouter(), tsconfigPaths()],
+  plugins: [
+    { enforce: 'pre', ...mdx() },
+    reactRouter(),
+    tsconfigPaths(),
+    netlifyPlugin(),
+  ],
 });
